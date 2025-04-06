0a35c551f42895e8787a08288ea80f70020b9e20
Junhyeok Seo <jun673012@gmail.com>
2025-04-06 17:40:07 +0900

    init

---
+++ src/test/java/com/jun/bidup/BidupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.bidup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
